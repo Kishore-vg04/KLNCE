de0f8dcfb2a0596cf6d8867b28215fc350b189fc
Madesh M <madeshdex10@gmail.com>
2026-03-24 18:37:07 +0530

    add a new route for department

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Footer from "./Component/Footer";
+import Admissions from "./Component/Department/Department";
 import {Routes,Route,BrowserRouter} from  'react-router-dom';
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About/>}></Route>
+          <Route path="/department" element={<Admissions/>}></Route>
         </Routes>
         <Footer/>
       </BrowserRouter>
